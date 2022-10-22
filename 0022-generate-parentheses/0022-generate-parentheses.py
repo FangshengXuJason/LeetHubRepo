d53c0fe44416b6dbfd5d70cf3519b609c45cc3c7
Fangsheng Xu/Jason <91645998+FangshengXuJason@users.noreply.github.com>
2022-10-21 20:09:04 -0700

    Time: 49 ms (77.21%), Space: 14.1 MB (77.06%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,9 +1,18 @@
-class Solution(object):
-    def generateParenthesis(self, N):
-        if N == 0: return ['']
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
         ans = []
-        for c in range(N):
-            for left in self.generateParenthesis(c):
-                for right in self.generateParenthesis(N-1-c):
-                    ans.append('({}){}'.format(left, right))
+        def backtrack(S = [], left = 0, right = 0):
+            if len(S) == 2 * n:
+                ans.append("".join(S))
+                return
+            if left < n:
+                S.append("(")
+                backtrack(S, left+1, right)
+                S.pop()
+            if right < left:
+                S.append(")")
+                backtrack(S, left, right+1)
+                S.pop()
+        backtrack()
         return ans
+    
