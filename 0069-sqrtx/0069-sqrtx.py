93c5fe31adc31eca568ceb401082d6a7624fc516
Fangsheng Xu/Jason <91645998+FangshengXuJason@users.noreply.github.com>
2022-10-24 23:30:48 -0700

    Time: 45 ms (87.07%), Space: 13.8 MB (56.53%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,9 @@
+from math import e, log
+class Solution:
+    def mySqrt(self, x):
+        if x < 2:
+            return x
+        
+        left = int(e**(0.5 * log(x)))
+        right = left + 1
+        return left if right * right > x else right
