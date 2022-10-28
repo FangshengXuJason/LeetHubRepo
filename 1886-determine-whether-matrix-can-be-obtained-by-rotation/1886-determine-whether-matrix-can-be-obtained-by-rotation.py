181fa8effc8311133b6d5a76a7e95f65ab744990
Fangsheng Xu/Jason <91645998+FangshengXuJason@users.noreply.github.com>
2022-10-27 20:33:29 -0700

    Time: 91 ms (29.20%), Space: 13.9 MB (78.51%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.py
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.py
@@ -1,6 +1,27 @@
 class Solution:
     def findRotation(self, mat: List[List[int]], target: List[List[int]]) -> bool:
-        for _ in range(4): 
-            if mat == target: return True
-            mat = [list(x) for x in zip(*mat[::-1])]
-        return False 
+        if mat == target:
+            return True
+        
+        for i in range(3):
+            self.rotate(mat)
+            if mat == target:
+                return True
+        return False
+
+    def swap(self, matrix, i, j, i1, j1):
+        tmp = matrix[i][j]
+        matrix[i][j] = matrix[i1][j1]
+        matrix[i1][j1] = tmp
+    
+    def rotate(self, matrix):
+        R = len(matrix)
+        C = len(matrix[0])
+        for i in range(R):
+            for j in range(C//2):
+                self.swap(matrix, i, j, i, C - j - 1)
+        # print(f"{matrix}")
+        for i in range(R):
+            for j in range(i + 1, C):
+                self.swap(matrix, i, j, j, i)
+        # print(f"rotated: {matrix}")
