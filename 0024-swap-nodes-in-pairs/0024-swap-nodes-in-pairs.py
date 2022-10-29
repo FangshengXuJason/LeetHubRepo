4ddfc22fc7925a55ef0e77f01dc36ceefc537b57
Fangsheng Xu/Jason <91645998+FangshengXuJason@users.noreply.github.com>
2022-10-28 17:10:55 -0700

    Time: 72 ms (8.52%), Space: 13.9 MB (65.13%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -16,13 +16,8 @@ class Solution(object):
         if not head or not head.next:
             return head
 
-        # Nodes to be swapped
-        first_node = head
-        second_node = head.next
-
-        # Swapping
-        first_node.next  = self.swapPairs(second_node.next)
-        second_node.next = first_node
-
-        # Now the head is the second node
-        return second_node
+        save = head.next.next;
+        ret = head.next;
+        head.next.next = head;
+        head.next = self.swapPairs(save);
+        return ret;
