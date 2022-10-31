b4cf9e44957f0b0d8e08c860332a9647be7966b5
Fangsheng Xu/Jason <91645998+FangshengXuJason@users.noreply.github.com>
2022-10-30 20:23:20 -0700

    Time: 98 ms (27.02%), Space: 68.6 MB (18.17%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -3,31 +3,23 @@ enum Index {
 }
 
 public class Solution {
-    Index[] memo;
-
-    public boolean canJumpFromPosition(int position, int[] nums) {
-        if (memo[position] != Index.UNKNOWN) {
-            return memo[position] == Index.GOOD ? true : false;
-        }
-
-        int furthestJump = Math.min(position + nums[position], nums.length - 1);
-        for (int nextPosition = position + 1; nextPosition <= furthestJump; nextPosition++) {
-            if (canJumpFromPosition(nextPosition, nums)) {
-                memo[position] = Index.GOOD;
-                return true;
-            }
-        }
-
-        memo[position] = Index.BAD;
-        return false;
-    }
-
     public boolean canJump(int[] nums) {
-        memo = new Index[nums.length];
+        Index[] memo = new Index[nums.length];
         for (int i = 0; i < memo.length; i++) {
             memo[i] = Index.UNKNOWN;
         }
         memo[memo.length - 1] = Index.GOOD;
-        return canJumpFromPosition(0, nums);
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            int furthestJump = Math.min(i + nums[i], nums.length - 1);
+            for (int j = i + 1; j <= furthestJump; j++) {
+                if (memo[j] == Index.GOOD) {
+                    memo[i] = Index.GOOD;
+                    break;
+                }
+            }
+        }
+
+        return memo[0] == Index.GOOD;
     }
 }
