102a36ec8cc2759186e3a7af4747db15189dfe80
Fangsheng Xu/Jason <91645998+FangshengXuJason@users.noreply.github.com>
2022-10-30 21:06:30 -0700

    Time: 7 ms (49.66%), Space: 68.4 MB (29.49%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,12 +1,11 @@
-class Solution {
+public class Solution {
     public boolean canJump(int[] nums) {
-        int last_good_index = nums.length-1;
-        
-        for(int i=nums.length-1;i>=0;i--){
-            if(i+nums[i]>=last_good_index){
-                last_good_index = i;
+        int lastPos = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
             }
         }
-        return last_good_index == 0;
+        return lastPos == 0;
     }
 }
